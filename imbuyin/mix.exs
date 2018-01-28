3e1283020cd9216ed7ea9697fd40a53fc7edd45c
Graham Leva <g.ravel66@gmail.com>
2018-01-27 23:00:24 -0600

    Bump Elixir version to 1.6

--- imbuyin/mix.exs
+++ imbuyin/mix.exs
@@ -5,7 +5,7 @@ defmodule Imbuyin.Mixfile do
     [
       app: :imbuyin,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
